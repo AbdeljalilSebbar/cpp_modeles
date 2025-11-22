e513f6cdc2e2f94a07ee10a88b996e963824f7fa
Abdeljalil Sebbar <abdsebba@e3r9p4.1337.ma>
2025-11-22 18:57:00 +0100

    finish

--- cpp_model_06/ex01/main.cpp
+++ cpp_model_06/ex01/main.cpp
@@ -2,29 +2,36 @@
 
 int main()
 {
-    Data* data = new Data(42, "Network");
-    std::cout << "Original Data address on STACK: " << &data << std::endl;
-    std::cout << "Original Data address on HEAP: " << data << std::endl;
-    std::cout << "Original Data content: "
-              << data->Id << ", " << data->name << std::endl;
+	Data* data;
+	try
+	{
+		data = new Data(42, "Network");
+		std::cout << "Original Data address on STACK: " << &data << std::endl;
+		std::cout << "Original Data address on HEAP: " << data << std::endl;
+		std::cout << "Original Data content: "
+				<< data->Id << ", " << data->name << std::endl;
 
-    std::cout << "size of uintptr_t: " << sizeof(uintptr_t) << std::endl;
+		std::cout << "size of uintptr_t: " << sizeof(uintptr_t) << std::endl;
 
-    // Serialize
-    std::cout << "==================================================" << std::endl;
-    uintptr_t raw = Serializer::serialize(data);
-    std::cout << "Serialized value (uintptr_t): " << raw << std::endl;
-    std::cout << "==================================================" << std::endl;
+		// Serialize
+		std::cout << "==================================================" << std::endl;
+		uintptr_t raw = Serializer::serialize(data);
+		std::cout << "Serialized value (uintptr_t): " << raw << std::endl;
+		std::cout << "==================================================" << std::endl;
 
-    std::cout << "==================================================" << std::endl;
-    // Deserialize
-    Data* restored = Serializer::deserialize(raw);
-    std::cout << "Restored Data address on STACK: " << &restored << std::endl;
-    std::cout << "Restored Data address on HEAP: " << restored << std::endl;
-    std::cout << "Restored Data content: "
-              << restored->Id << ", " << restored->name << std::endl;
-    std::cout << "==================================================" << std::endl;
-
-    delete data;
-    return 0;
+		std::cout << "==================================================" << std::endl;
+		// Deserialize
+		Data* restored = Serializer::deserialize(raw);
+		std::cout << "Restored Data address on STACK: " << &restored << std::endl;
+		std::cout << "Restored Data address on HEAP: " << restored << std::endl;
+		std::cout << "Restored Data content: "
+				<< restored->Id << ", " << restored->name << std::endl;
+		std::cout << "==================================================" << std::endl;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	delete data;
+	return 0;
 }
